8338c01c0e9cd05032306176910469c343cdab65
Aleksei Flis <fliseno1k@gmail.com>
2020-03-19 02:41:35 +1000

    refactor

--- js/mess.js
+++ js/mess.js
@@ -1,73 +1,76 @@
 L.Map.addInitHook(function () {
-	var map = this,
-		data = {},
-		gmxMap = null,
-		lid = '9D36AA7EAD0244F5BFBCF7D55453FED6',
-		mid = '5d4c240a69caa174d2aed035915c95cc',
-		delay = 6000,
-	    	prefix = '//maps.kosmosnimki.ru',
-		mess = L.geoJSON([], {
-			onEachFeature: function (feature, layer) {
-				let props = feature.properties;
-				data[props.id] = layer;
-				layer.options.icon = L.icon({
-					iconUrl: prefix + '/GetImage.ashx?usr=motorin%40scanex.ru&img=MESSAGE.png',
-					iconSize: [25, 25],
-					shadowSize:   [27, 27],
-					iconAnchor: [15, 15],
-					popupAnchor: [-4, -20]
-				});
-			}
-		}).bindPopup(function (layer) {
-		if (Math.floor((new Date() -  new Date(layer.feature.properties["Date"])) / 3600000) == 0) {
-                var pri = 'только что'
-                }
-                else if  (Math.floor((new Date() -  new Date(layer.feature.properties["Date"])) / 3600000) == 1) {
-                var pri = 'час назад'
-                }
-                else if (Math.floor((new Date() -  new Date(layer.feature.properties["Date"])) / 3600000) > 1 & Math.floor((new Date() -  new Date(layer.feature.properties["Date"])) / 3600000) < 5 ) {
-                var pri = Math.floor((new Date() -  new Date(layer.feature.properties["Date"])) / 3600000) + ' часа назад'
-                }
-                else if (Math.floor((new Date() -  new Date(layer.feature.properties["Date"])) / 3600000) >= 5) {
-                var pri = Math.floor((new Date() -  new Date(layer.feature.properties["Date"])) / 3600000) + ' часов назад'
-                }
-			var textInPopup = JSON.stringify(layer.feature.properties["mess"], null, 2).replace(/\\"/g, "'").replace(/\\n/g, '<br>').replace(/\"/g, '')
-			var Text = textInPopup.replace(/"/g, '')
-			var clearText = Text.replace(/n/g, '<br>')
-			return '<b><p align="justify">' +  textInPopup  + '</b></p>'  + '<br>Сообщение оставленно ' 
-				+ pri;
-		}),
-		reget = () => {
-		 if (mess._map) {
-			 fetch('getmess.php')
-				.then((res) => res.json())
-				.then((arr) => {
-					arr.forEach(it => {
-						let feature = {
-							type: 'Feature',
-							geometry: {
-								type: 'Point',
-								coordinates: [it.latlng["lng"],it.latlng["lat"]]
-							},
-							properties: it
-						};
-						let layer = data[it.id];
-						if (layer) {
-							if (((new Date() - new Date(it.Date)) / 3600000) > 12) {
-								layer.remove([it.id])
-							}
-							else {
-								layer.setLatLng([it.latlng["lat"],it.latlng["lng"]]);
-							}
-						} else {
-							if (((new Date() - new Date(it.Date)) / 3600000) < 12) {
-							mess.addData([feature]);
-							}
+	var map = this;
+	var data = {};
+	var gmxMap = null;
+	const lid = '9D36AA7EAD0244F5BFBCF7D55453FED6';
+	const mid = '5d4c240a69caa174d2aed035915c95cc';
+	const delay = 6000;
+	const prefix = '//maps.kosmosnimki.ru';
+
+	var mess = L.geoJSON([], {
+		onEachFeature: function (feature, layer) {
+			let props = feature.properties;
+			data[props.id] = layer;
+			layer.options.icon = L.icon({
+				iconUrl: prefix + '/GetImage.ashx?usr=motorin%40scanex.ru&img=MESSAGE.png',
+				iconSize: [25, 25],
+				shadowSize:   [27, 27],
+				iconAnchor: [15, 15],
+				popupAnchor: [-4, -20]
+			});
+		}
+	}).bindPopup(function (layer) {
+		var timeOfAdd = Math.floor((new Date() -  new Date(layer.feature.properties["Date"])) / 3600000);
+		var pri = timeOfAdd.toString();
+
+		if (timeOfAdd == 0) {
+			pri.concat('только что');
+		} else if (timeOfAdd == 1) {
+			pri.concat('час назад');
+		} else if (timeOfAdd > 1 & timeOfAdd < 5 ) {
+			pri.concat(' часа назад');
+		} else if (timeOfAdd >= 5) {
+			pri.concat(' часов назад');
+		}
+
+		var textInPopup = JSON.stringify(layer.feature.properties["mess"], null, 2).replace(/\\"/g, "'")
+																				   .replace(/\\n/g, '<br>')
+																				   .replace(/\"/g, '');
+
+		return '<b><p align="justify">' +  textInPopup  + '</b></p>'  + '<br>Сообщение оставленно ' + pri;
+	});
+
+	var reget = () => {
+		if (mess._map) {
+			fetch('getmess.php')
+			.then((res) => res.json())
+			.then((arr) => {
+				arr.forEach(it => {
+					let feature = {
+						type: 'Feature',
+						geometry: {
+							type: 'Point',
+							coordinates: [it.latlng["lng"],it.latlng["lat"]]
+						},
+						properties: it
+					};
+					let layer = data[it.id];
+					if (layer) {
+						if (((new Date() - new Date(it.Date)) / 3600000) > 12) {
+							layer.remove([it.id])
 						}
-					})
-			 });
-		 }
-		};
+						else {
+							layer.setLatLng([it.latlng["lat"],it.latlng["lng"]]);
+						}
+					} else {
+						if (((new Date() - new Date(it.Date)) / 3600000) < 12) {
+						mess.addData([feature]);
+						}
+					}
+				})
+			});
+		}
+	};
 
 	map.on('layeradd', (ev) => {
 		if (!gmxMap && L.gmx._maps['maps.kosmosnimki.ru']) {
@@ -80,10 +83,9 @@ L.Map.addInitHook(function () {
 			if (gmxProps.visible) {
 				map.addLayer(mess);
 			}
-			// console.log('dddf', fg);
+	
 			gmxMap.layersByID[lid] = mess;
 			setInterval(reget, delay);
-
 			reget();
 		}
 	});
