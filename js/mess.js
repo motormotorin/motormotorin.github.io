9f3496a4130f52d1fb9696cd91d133eec4bcfa10
Aleksei Flis <fliseno1k@gmail.com>
2020-05-11 01:17:05 +1000

    .

--- js/mess.js
+++ js/mess.js
@@ -22,24 +22,6 @@ L.Map.addInitHook(function () {
 				click: openMessage(layer.feature.properties['mess'], layer.feature.properties['Date'])
 			});
 		}
-	}).bindPopup(function (layer) {
-		var timeOfAdd = Math.floor((new Date() -  new Date(layer.feature.properties["Date"])) / 3600000).toString();
-
-		if (timeOfAdd == '0') {
-			timeOfAdd = 'только что';
-		} else if (timeOfAdd == '1') {
-			timeOfAdd = 'час назад';
-		} else if (timeOfAdd > '1' && timeOfAdd < '5' ) {
-			timeOfAdd += ' часа назад';
-		} else if (timeOfAdd >= '5') {
-			timeOfAdd += ' часов назад';
-		}
-
-		var textInPopup = JSON.stringify(layer.feature.properties["mess"], null, 2).replace(/\\"/g, "'")
-																				   .replace(/\\n/g, '<br>')
-																				   .replace(/\"/g, '');
-
-		return '<b><p align="justify">' +  textInPopup  + '</b></p>'  + '<br>Сообщение оставленно ' + timeOfAdd;
 	});
 
 	var reget = () => {
