ad142dbd563b74b258b4e6181adfb461b2fa0b45
Aleksei Flis <fliseno1k@gmail.com>
2020-07-08 16:28:42 +1000

    Add base DOM elements classes strings

---
+++ CampusGisAdmin/src/js/Util/Base.js
@@ -0,0 +1,30 @@
+export const headerDOM = {
+    header: "header",
+    headerUl: "header__ul",
+    headerLi: "header__li",
+    headerLiActive: "header__li--active"
+}
+
+export const editorBarDOM = {
+    editorBar: "left-sidebar",
+    selectorLi: "selectors__li",
+    selectorLiActive: "selectors__li--active",
+    layerSelectorID: "layers-slctr",
+    buildingsSelectorID: "buildings-slctr",
+
+    elementsToolbarID: "elements-toolbar",
+
+
+    containerShowBtn: "toolbar__toogle--show",
+    containerHideBtn: "toolbar__toogle--hide",
+}
+
+export const layersContainerDOM = {
+    toolbarID: "layers-toolbar",
+    addBtnID: "add-layer",
+    delBtnID: "del-layer",
+    container: "layers-container",
+    containerShow: "toolbar__toogle--show",
+    containerHide: "toolbar__toogle--hide",
+    acordionID: "acrd"
+}
