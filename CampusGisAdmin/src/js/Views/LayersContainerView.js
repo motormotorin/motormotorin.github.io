21819e0b55006f706d41a1d8f409822f5b305421
Aleksei Flis <fliseno1k@gmail.com>
2020-07-08 16:27:18 +1000

    Add layers container view controller

---
+++ CampusGisAdmin/src/js/Views/LayersContainerView.js
@@ -0,0 +1,9 @@
+
+const LayersContainerView = {
+    highlightSelectedLayer: function() {},
+    adLayer: function() {},
+    deleteLayer: function() {},
+    renderLayers: function() {}
+}
+
+export default LayersContainerView;
