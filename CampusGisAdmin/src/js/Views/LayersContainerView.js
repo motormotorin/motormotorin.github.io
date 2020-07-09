037316b438196521157eafb082c374ff9152ab1b
Aleksei Flis <fliseno1k@gmail.com>
2020-07-09 22:55:21 +1000

    Add new view methods
    * init method for settingup default object dom element
    * getInputValue method for getting input from item
    * setAsSaved/setAsUnsaved for changing layer's state img

--- CampusGisAdmin/src/js/Views/LayersContainerView.js
+++ CampusGisAdmin/src/js/Views/LayersContainerView.js
@@ -1,38 +1,65 @@
 import { layersContainerDOM } from '../Util/Base';
+import Layer from '../Models/Layer';
 
 
 
-const LayersContainerView = {
-    highlightSelectedLayer: function() {},
+const LayersContainerView = {};
 
-    addLayer: function(layer) {     
-        const layerHTML = `
-            <div id="${layer.id}" class="item-row">
-                <span class="item-row__img">
-                    <img src="./media/file.svg">
-                </span>
-                <span>
-                    <input class="item-row__input"type="text" placeholder="Название слоя" value="${layer.title}">
-                </span>
-                <button id="save" class="item-row__btn">
-                    <img src="./media/${layer.state === "new" ? "unsaved" : "saved"}.svg">
-                </button>
-            </div>
-        `;
-
-        document.querySelector(`.${layersContainerDOM.container}`).insertAdjacentHTML("afterbegin", layerHTML);
-    },
-
-    deleteLayer: function() {},
-    renderLayers: function() {},
-
-    toggleContainer: function() {
-        const toolbarToggler = document.querySelector(`#${layersContainerDOM.toolbarID} #${layersContainerDOM.acordionID}`);
-        const layersContainer = document.querySelector(`.${layersContainerDOM.container}`);
-
-        toolbarToggler.classList.toggle(layersContainerDOM.acordionHide);
-        layersContainer.classList.toggle(layersContainerDOM.containerHide);
-    }
+LayersContainerView.init = function() {
+    this.container = document.querySelector(`.${layersContainerDOM.container}`);
+    return this;
 }
+
+LayersContainerView.highlightSelectedLayer = function(layer) {
+    Array.from(this.container.children).forEach(chld => {
+        chld.classList.remove(layersContainerDOM.itemActive);
+    });
+    layer.classList.add(layersContainerDOM.itemActive);
+};
+
+LayersContainerView.renderLayer = function(layer) {     
+    const layerHTML = `
+        <div id="${layer.id}" class="item-row">
+            <span class="item-row__img">
+                <img src="./media/file.svg">
+            </span>
+            <span>
+                <input class="item-row__input"type="text" placeholder="Название слоя" value="${layer.title}" disabled>
+            </span>
+            <button id="save-btn" class="item-row__btn">
+                <img src="./media/${layer.state === "new" ? "unsaved" : "saved"}.svg">
+            </button>
+        </div>
+    `;
+
+    this.container.insertAdjacentHTML("afterbegin", layerHTML);
+};
+
+LayersContainerView.clearLayer = function(id) {
+    const layer = this.container.querySelector(`#${id}`);
+    this.container.removeChild(layer);
+}
+
+LayersContainerView.getInputValue = function(item) {
+    return item.querySelector('input').value || "";
+}
+
+LayersContainerView.setAsSaved = function(item) {
+    const img = item.querySelector(`#${layersContainerDOM.itemSaveBtnID} img`);
+    img.src = "./media/saved.svg";
+};
+
+LayersContainerView.setAsUnsaved = function(item) {
+    const img = item.querySelector(`#${layersContainerDOM.itemSaveBtnID} img`);
+    img.src = "./media/unsaved.svg";
+};
+
+LayersContainerView.toggleContainer = function() {
+    const toolbarToggler = document.querySelector(`#${layersContainerDOM.toolbarID} #${layersContainerDOM.acordionID}`);
+
+    toolbarToggler.classList.toggle(layersContainerDOM.acordionHide);
+    this.container.classList.toggle(layersContainerDOM.containerHide);
+};
+
 
 export default LayersContainerView;
