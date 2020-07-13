8e17133b105304079ef281f50576bb35ab170ebc
Aleksei Flis <fliseno1k@gmail.com>
2020-07-14 01:28:25 +1000

    Implement view methods
    * init
    * highlightSelectedElement
    * renderElement
    * clearElement
    * clearAll
    * toggleContainer

--- CampusGisAdmin/src/js/Views/ElementsContainerView.js
+++ CampusGisAdmin/src/js/Views/ElementsContainerView.js
@@ -1,15 +1,46 @@
+import { elementsContainerDOM, elementDOM } from '../Util/Base';
 
 
 const ElementsContainerView = {};
 
-ElementsContainerView.init = function() {}
+ElementsContainerView.init = function() {
+    this.container = document.querySelector(`.${elementsContainerDOM.container}`);
+    return this;
+}
 
-ElementsContainerView.highlightSelectedElement = function() {}
+ElementsContainerView.highlightSelectedElement = function(element) {
+    Array.from(this.container.children).forEach(chld => {
+        chld.classList.remove(elementDOM.itemActive);
+    });
+    element.classList.add(elementDOM.itemActive);
+}
 
-ElementsContainerView.renderElement = function() {}
+ElementsContainerView.renderElement = function(element) {
+    const elementHTML = `
+        <div id="${element.id}" class="item-row item-row--flex-start">
+            <img class="item-row__img" src="./media/file.svg">
+            <input class="item-row__input item-row__input--margin-left" type="text" 
+                placeholder="Элемент" 
+                value="" disabled>
+        </div>
+    `;
 
-ElementsContainerView.clearElement = function() {}
+    this.container.insertAdjacentHTML("afterbegin", elementHTML);
+}
 
-ElementsContainerView.clearAll = function() {}
+ElementsContainerView.clearElement = function(element) {
+    element.parentNode.removeChild(element);
+}
+
+ElementsContainerView.clearAll = function() {
+    this.container.innerHTML = "";
+}
+
+ElementsContainerView.toggleContainer = function() {
+    const toolbarToggler = document.querySelector(`#${elementsContainerDOM.acordionID}`);
+
+    toolbarToggler.classList.toggle(elementsContainerDOM.acordionHide);
+    this.container.classList.toggle(elementsContainerDOM.containerHide);
+}
 
 export default ElementsContainerView;
