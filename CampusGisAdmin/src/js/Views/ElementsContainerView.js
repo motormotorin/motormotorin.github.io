0c9d369429fa47e3597ade3df618233db5673589
Aleksei Flis <fliseno1k@gmail.com>
2020-07-13 16:30:25 +1000

    Add elements container view controller
    Write default list of functions for this view controller

---
+++ CampusGisAdmin/src/js/Views/ElementsContainerView.js
@@ -0,0 +1,15 @@
+
+
+const ElementsContainerView = {};
+
+ElementsContainerView.init = function() {}
+
+ElementsContainerView.highlightSelectedElement = function() {}
+
+ElementsContainerView.renderElement = function() {}
+
+ElementsContainerView.clearElement = function() {}
+
+ElementsContainerView.clearAll = function() {}
+
+export default ElementsContainerView;
