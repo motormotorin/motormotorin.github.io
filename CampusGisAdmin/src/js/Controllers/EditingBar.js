895a61183e90a12b05919f28d683b707840faa0c
Aleksei Flis <fliseno1k@gmail.com>
2020-07-20 23:49:47 +1000

    Delete all dom objects and rewrite call of dom strings
    All dom strings and elements replaced to view of this controller

--- CampusGisAdmin/src/js/Controllers/EditingBar.js
+++ CampusGisAdmin/src/js/Controllers/EditingBar.js
@@ -1,8 +1,13 @@
 import EditingBarView from '../Views/EditingBarView';
+
 import LayersContainer from '../Controllers/LayersContainer';
+import ElementsContainer from '../Controllers/ElementsContainer';
+import MarkerEditingBar from './MarkerEditingBar';
+
+
 import { editorBarDOM } from '../Util/Base';
 
-function EditingBar(map, data) {
+function EditingBar(map, mapData) {
     this.selectedType = "layers";
 
     this.typesToId = {
@@ -11,12 +16,12 @@ function EditingBar(map, data) {
     }
 
     this._map = map;
-    this._data = data || [];
-    this._edBarView = EditingBarView;
+    this._mapData = mapData;
+    this._edBarView = EditingBarView.init();
 
-    this._layersContainer = new LayersContainer();
-    this._elementsContainer = undefined;
-    
+    this._layersContainer = new LayersContainer(this, []);
+    this._elementsContainer = new ElementsContainer(this, []);
+    this._rightBar = new MarkerEditingBar();
     this._edBarView.show();
     this.initHandlers();
     this.selectType(this.selectedType);
@@ -26,16 +31,32 @@ EditingBar.prototype.initHandlers = function() {
     this._edBarView.editorBar.addEventListener("click", (e) => {
         e.preventDefault();
         
-        if (e.target.closest(`.${editorBarDOM.selectorLi}`)) {
-            const type = Object.keys(this.typesToId).find(key => this.typesToId[key] === e.target.id);
+        if (e.target.closest(`.${this._edBarView.DOMstrings.selectorLi}`)) {
+            const type = Object.keys(this.typesToId)
+                .find(key => this.typesToId[key] === e.target.id);
+                
             type ? this.selectType(type) : void 0;
         } 
+    });
+
+    this._layersContainer.on("layerSelected", () => {
+        console.log("layer selected");
+        this._elementsContainer.enable();
+    });
+
+    this._layersContainer.on("layerAdded", () => {
+        console.log("layer added");
+    });
+
+    this._layersContainer.on("layerDeleted", () => {
+        console.log("layer deleted");
+        this._elementsContainer.disable();
     });
 }
 
 EditingBar.prototype.selectType = function(type) {
-    // -- const arrayOfType = this._data.getObjectArrayByType(type)
-    // -- this.layersContainer.render(arrayOfType);
+    // const data = this._mapData.getData(type);
+    // this._layersContainer.render(data);
     this.selectedType = type;
     this._edBarView.highlightSelected(this.typesToId[type]);
 }
