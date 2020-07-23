ccebe7897df83d35a8d073fce2629a138ec144c4
Aleksei Flis <fliseno1k@gmail.com>
2020-07-23 19:36:44 +1000

    Refactor and change dom links

--- CampusGisAdmin/src/js/Controllers/LayersContainer.js
+++ CampusGisAdmin/src/js/Controllers/LayersContainer.js
@@ -1,119 +1,140 @@
-import { layersContainerDOM, elementDOM } from '../Util/Base';
+import { 
+    layersContainerDOM as elements, 
+    elementDOM as item 
+} from '../Util/Base';
+
 import LayersContainerView from '../Views/LayersContainerView';
+
 import Layer from '../Models/Layer';
 import Event from '../Util/Event';
 
 
-function LayersContainer(editingBar, data) {
+function LayersContainer(data) {
     this.layers = data || [];
-    this.selectedLayer = null;
-
-    this._editingBar = editingBar;
-    this._lContView = LayersContainerView.init();
+    this._lContView = new LayersContainerView();
+    
     this.initHandlers();
 } 
 
 LayersContainer.prototype = Object.create(Event.prototype);
 
 LayersContainer.prototype.initHandlers = function() {
-    this.container = document.querySelector(`.${layersContainerDOM.container}`);
-    this.toolbar = document.querySelector(`#${layersContainerDOM.toolbarID}`);
 
-    this.toolbar.addEventListener("click", (e) => {
-        if (e.target.closest(`#${layersContainerDOM.acordionID}`)) {
+    // toolbar events: toggle btn; add layer; delete layer;
+    this._lContView.toolbar.addEventListener("click", (e) => {
+        if (e.target.closest(`.${elements.toolbarToggleBtn}`)) {
             this._lContView.toggleContainer();
 
-        } else if (e.target.closest(`#${layersContainerDOM.addBtnID}`)) {
+        } else if (e.target.closest(`#${elements.addBtnID}`)) {
             this.addNewLayer();
 
-        } else if (e.target.closest(`#${layersContainerDOM.delBtnID}`)) {
-            this.delLayer();
+        } else if (e.target.closest(`#${elements.delBtnID}`)) {
+            this.deleteLayer();
         } 
     });
 
-    this.container.addEventListener("click", (e) => {
-        if (e.target.closest(`#${elementDOM.itemSaveBtnID}`)) {
-            let item = e.target.closest(`.${elementDOM.item}`);
-            this.saveLayer(item);
-        } else if (e.target.closest(`.${elementDOM.item}`)) {
-            let item = e.target.closest(`.${elementDOM.item}`);
-            this.selectLayer(item);
+    // container events: select layer; save layer;
+    this._lContView.container.addEventListener("click", (e) => {
+        if (e.target.closest(`#${item.itemSaveBtnID}`)) {
+            let layerId = e.target.closest(`.${item.item}`).id;
+            this.saveLayer(layerId);
+
+        } else if (e.target.closest(`.${item.item}`)) {
+            let layerId = e.target.closest(`.${item.item}`).id;
+            this.selectLayer(layerId);
         }
     });
 
-    this.container.addEventListener("dblclick", (e) => {
-        if (e.target.className === elementDOM.itemInput) {
-            let item = e.target.closest(`.${elementDOM.item}`);
+    // container events: enable input of element row
+    this._lContView.container.addEventListener("dblclick", (e) => {
+        if (e.target.className === item.itemInput) {
+            let layerId = e.target.closest(`.${item.item}`).id;
+
             e.target.disabled = false;
             e.target.focus();
-            this._lContView.highlightSelectedLayer(item);
+
+            this._lContView.highlightSelectedLayer(layerId);
         }
     });
 
-    this.container.addEventListener("input", (e) => {
-       const item = e.target.closest(`.${elementDOM.item}`);
-       const layer = this.layers.find(layer => layer.id === item.id);
-       layer.title === e.target.value ? this._lContView.setAsSaved(item) 
-        : this._lContView.setAsUnsaved(item);
+    // container events: change value of layer with input value and set state(saved, unsaved);
+    this._lContView.container.addEventListener("input", (e) => {
+        const layerId = e.target.closest(`.${item.item}`).id;
+        const layer = this.layers.find(layer => layer.id === layerId);
+
+        layer.title === e.target.value 
+            ? this._lContView.setAsSaved(layerId) 
+            : this._lContView.setAsUnsaved(layerId);
     });
 
-    this.container.addEventListener("change", (e) => {
+    // container events: on change input value disable  
+    this._lContView.container.addEventListener("change", (e) => {
         e.target.blur();
         e.target.disabled = true;
     });
 
-    this.container.addEventListener("focusout", (e) => {
+    // container events: on focusout from layer element
+    this._lContView.container.addEventListener("focusout", (e) => {
         e.target.disabled = true;
     });
 }
 
 
 LayersContainer.prototype.update = function(newlayers) {
+    this._lContView.clearContainer();
     this.layers = newlayers;
-    this.selectedLayer = null;
-    this._lContView.clearAll();
-
     this.layers.forEach(layer => {
         this._lContView.renderLayer(layer);
     });
 }
 
-LayersContainer.prototype.selectLayer = function(item) {
-    this.emit("layerSelected", []);
-    this.selectedLayer = item;
-    this._lContView.highlightSelectedLayer(item);
+LayersContainer.prototype.selectLayer = function(layerId) {
+    this._lContView.highlightSelectedLayer(layerId);
+
+    const layer = this.layers.find(layer => layer.id === layerId);
+    if (layer.state == "saved") {
+        this.emit("layerSelected", []);
+    }
 }
 
 LayersContainer.prototype.addNewLayer = function() {
-    this.emit("layerAdded", []);
     const newLayer = new Layer();
+
     this.layers.push(newLayer);
     this._lContView.renderLayer(newLayer);
+
+    this.emit("layerAdded", []);
 }
 
-LayersContainer.prototype.saveLayer = function(item) {
-    const layer = this.layers.find(layer => layer.id === item.id);
-    layer.title = this._lContView.getInputValue(item);
-    this._lContView.setAsSaved(item);
+LayersContainer.prototype.saveLayer = function(layerId) {
+    const layer = this.layers.find(layer => layer.id === layerId);
+    layer.title = this._lContView.getInputValue(layerId);
+    layer.state = "saved";
 
+    this._lContView.setAsSaved(layerId);
+
+    if (layer.id === this._lContView.getSelectedLayerId()) {
+        this.emit("layerSelected", []);
+    }
     // -- maker server request
     // -- play animation 
     // -- on succes changeSaveIcon
     // -- on faoilure show error
 }
 
-LayersContainer.prototype.delLayer = function() {
-    this.emit("layerDeleted");
-    if (this.selectedLayer === null || this.selectedLayer === undefined) return;
+LayersContainer.prototype.deleteLayer = function() {
+    const selectedLayerId = this._lContView.getSelectedLayerId();
+    const selectedLayer = this.layers.find(layer => layer.id === selectedLayerId);
 
-    const layer = this.layers.find(layer => layer.id === this.selectedLayer.id);
-    if (layer.state === "new") {
-        this.layers = this.layers.filter(l => l.id !== layer.id);
-        this._lContView.clearLayer(this.selectedLayer);
-        this.selectedLayer = null;
-        
-    } else {
-        console.log("server request on deleting");
+    if (selectedLayer) {
+        if (selectedLayer.state === "new") {
+            this.layers = this.layers.filter(layer => layer.id !== selectedLayer.id);
+            this._lContView.clearLayer(selectedLayerId);
+
+            this.emit("layerDeleted");
+        } else {
+            console.log("server request on deleting");
+        }
     }
 } 
 
