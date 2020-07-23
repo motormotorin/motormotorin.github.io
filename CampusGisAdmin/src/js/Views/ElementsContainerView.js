d0f72c40b3db8ac72b6a6a4e023f52d45cf20159
Aleksei Flis <fliseno1k@gmail.com>
2020-07-23 19:34:33 +1000

    Add new methods
    * enable/disable
    * isElementSelected
    * getSelectedElementId
    * renderElement
    * clearContainer
    * clearContainer

--- CampusGisAdmin/src/js/Views/ElementsContainerView.js
+++ CampusGisAdmin/src/js/Views/ElementsContainerView.js
@@ -1,46 +1,79 @@
-import { elementsContainerDOM, elementDOM } from '../Util/Base';
+import { 
+    elementsContainerDOM as elements, 
+    elementDOM as row 
+} from '../Util/Base';
 
 
-const ElementsContainerView = {};
-
-ElementsContainerView.init = function() {
-    this.container = document.querySelector(`.${elementsContainerDOM.container}`);
-    return this;
+function ElementsContainerView() {
+    this.container = document.querySelector(`.${elements.container}`);
+    this.toolbar = document.querySelector(`#${elements.toolbarID}`);
 }
 
-ElementsContainerView.highlightSelectedElement = function(element) {
-    Array.from(this.container.children).forEach(chld => {
-        chld.classList.remove(elementDOM.itemActive);
+ElementsContainerView.prototype.disable = function() {
+    const wrapper = `<div class="wrapper"></div>`;
+
+    this.toolbar.insertAdjacentHTML("afterbegin", wrapper);
+    this.container.insertAdjacentHTML("afterbegin", wrapper);
+}
+
+ElementsContainerView.prototype.enable = function() {
+    [this.toolbar, this.container].forEach(block => {
+        const child = block.querySelector('.wrapper');
+        if (child) child.parentNode.removeChild(child);
     });
-    element.classList.add(elementDOM.itemActive);
 }
 
-ElementsContainerView.renderElement = function(element) {
+ElementsContainerView.prototype.highlightSelectedElement = function(elementId) {
+    const elements = Array.from(this.container.children);
+    const selectedElement = elements.find(element => element.id === elementId);
+
+    elements.forEach(element => {
+        element.classList.remove(row.itemActive);
+    });
+    selectedElement.classList.add(row.itemActive);
+}
+
+ElementsContainerView.prototype.isElementSelected = function(elementId) {
+    return this.container.querySelector(`#${elementId}`).classList.contains(row.itemActive);
+}
+
+ElementsContainerView.prototype.getSelectedElementId = function() {
+    const selectedElement = this.container.querySelector(`.${row.itemActive}`);
+    return selectedElement 
+        ? selectedElement.id
+        : undefined;
+}
+
+
+ElementsContainerView.prototype.renderElement = function(element) {
     const elementHTML = `
-        <div id="${element.id}" class="item-row item-row--flex-start">
+        <div id="${element.id}" class="item-row">
             <img class="item-row__img" src="./media/file.svg">
-            <input class="item-row__input item-row__input--margin-left" type="text" 
-                placeholder="Элемент" 
-                value="" disabled>
+            <input class="item-row__input"type="text" 
+                placeholder="Название слоя" 
+                value="${element.title}" disabled>
+            <button id="edit-btn" class="item-row__btn">
+                <img src="./media/edit.svg">
+            </button>
         </div>
     `;
 
     this.container.insertAdjacentHTML("afterbegin", elementHTML);
 }
 
-ElementsContainerView.clearElement = function(element) {
+ElementsContainerView.prototype.clearElement = function(element) {
     element.parentNode.removeChild(element);
 }
 
-ElementsContainerView.clearAll = function() {
+ElementsContainerView.prototype.clearContainer = function() {
     this.container.innerHTML = "";
 }
 
-ElementsContainerView.toggleContainer = function() {
-    const toolbarToggler = document.querySelector(`#${elementsContainerDOM.acordionID}`);
-
-    toolbarToggler.classList.toggle(elementsContainerDOM.acordionHide);
-    this.container.classList.toggle(elementsContainerDOM.containerHide);
+ElementsContainerView.prototype.toggleContainer = function() {
+    const toolbarBtn = this.toolbar.querySelector(`.${elements.toolbarToggleBtn}`);
+    
+    toolbarBtn.classList.toggle(elements.acordionHide);
+    this.container.classList.toggle(elements.containerHide);
 }
 
 export default ElementsContainerView;
