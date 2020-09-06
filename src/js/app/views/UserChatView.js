8b2847b13e9aaa36275f8affdd543980038e52af
Aleksei Flis <fliseno1k@gmail.com>
2020-09-06 16:16:45 +1000

    add message block builder

--- src/js/app/views/UserChatView.js
+++ src/js/app/views/UserChatView.js
@@ -5,47 +5,46 @@ class UseChatView {
         this.$messageBtn = document.querySelector(".btns-group #message-btn");
     }
 
-    createMsgBlockHTML() {
-        var messageBlockHTML = `
-            <div class="message-block">
-                <div class="message-block__container shadow">
-                    <div class="writable-message">
-                        <textarea name="mess" 
-                            id="mess" 
-                            onlyread="false" 
-                            placeholder="Введите сообщение" 
-                            maxlength="110" 
-                            cols="30" rows="1"></textarea>
-                        <div class="user-btn user-btn--small shadow">
-                            <img src="${messageIcon}" alt="">
-                        </div>
+    buildMessageBlock() {
+        this.$messageBlock = this.createMessageBlock();
+        this.$messageBlock.innerHTML = this.fillMessageBlock();
+        this.render();
+    }
+
+    createMessageBlock() {
+        var messageBlock = document.createElement("div");
+        messageBlock.classList.add("message-block");
+        return messageBlock;
+    }
+
+    fillMessageBlock() {
+        return `
+            <div class="message-block__container shadow">
+                <div class="writable-message">
+                    <textarea name="mess" 
+                        id="mess" 
+                        onlyread="false" 
+                        placeholder="Введите сообщение" 
+                        maxlength="110" 
+                        cols="30" rows="1"></textarea>
+                    <div id="send-btn" class="user-btn user-btn--small shadow">
+                        <img src="${messageIcon}" alt="">
                     </div>
                 </div>
             </div>`;
-
-        return messageBlockHTML;
     }
 
-    async renderMsgBlock(html) {
-        await document.querySelector(".app .main .container")
-            .insertAdjacentHTML("beforeend", html);
-        
-        this.$messageBlock = document.querySelector(".message-block");
-        this.$sendMessageBtn = this.$messageBlock.querySelector(".user-btn");
-        
-        setTimeout(() => {
-            this.$messageBlock.classList.add("show");
-        }, 100);
+    render() {
+        document.querySelector(".app .main .container").insertAdjacentElement("beforeend", this.$messageBlock);
+        this.$messageBlock.classList.add("show");
     }
 
-    removeMsgBlock() {
+    removeMessageBlock() {
         try {
             this.$messageBlock.classList.remove("show");
 
             setTimeout(() => {
                 this.$messageBlock.parentNode.removeChild(this.$messageBlock);
-                this.$messageBlock = undefined;
-                this.$sendMessageBtn = undefined;
             }, 300);
 
         } catch(e) {
